4f1c25cea84ea8bcbdaee74c2bdb85124df0955f
ajujawade <ajayjawade06@gmail.com>
2024-10-15 01:29:47 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Projects from "./components/Projects";
 import Contact from './components/Contact'
 import Aos from "aos";
 import "aos/dist/aos.css"
+import { Analytics } from "@vercel/analytics/react"
 
 
 const App = () => {
